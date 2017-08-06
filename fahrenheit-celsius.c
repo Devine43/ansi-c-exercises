6436bec529eac50a971699460e6389f7ac219f2a
John Devine <john.devine1776@gmail.com>
2017-08-06 21:15:27 +0100

    Added headings to table.

--- fahrenheit-celsius.c
+++ fahrenheit-celsius.c
@@ -10,9 +10,10 @@ int main(){
 
     fahr = lower;
 
+    printf("Fahrenheit\tCelsius\n");
     while(fahr <= upper){
         celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f\t%6.1f\n", fahr,celsius);
+        printf("%3.0f\t\t%6.1f\n", fahr,celsius);
         fahr += step;
     }
 }
